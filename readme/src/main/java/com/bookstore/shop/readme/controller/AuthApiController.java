ea1c3baa2848bc498a66c144d99e57dc61ad4d37
hwandroid921 <olgksgml@gmail.com>
2026-03-28 19:08:27 +0900

    20260328 fix diagram and entity commit

--- readme/src/main/java/com/bookstore/shop/readme/controller/AuthApiController.java
+++ readme/src/main/java/com/bookstore/shop/readme/controller/AuthApiController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class AuthController {
+public class AuthApiController {
     private final MemberService memberService;
 
     // 로그인
