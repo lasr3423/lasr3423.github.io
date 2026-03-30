10e8ddcd81939231702388a1ebd42d0cc103aa27
baekks93 <baekks93@gmail.com>
2026-03-30 12:47:19 +0900

    [2026.03.30, seo] entity 마무리 및 Repository 생성!(아직 내용은 미작성)

--- readme/src/main/java/com/bookstore/shop/readme/domain/Payment.java
+++ readme/src/main/java/com/bookstore/shop/readme/domain/Payment.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
@@ -20,7 +22,6 @@ public class Payment extends BaseEntity {
     private Order order;
 
     @Enumerated(EnumType.STRING)
-    @ColumnDefault("null")
     private PaymentProvider paymentProvider;
 
     @Column(nullable = false, length = 30)
@@ -33,6 +34,25 @@ public class Payment extends BaseEntity {
     @Column(nullable = false)
     private int amount;
 
+    @Column(length = 100)
+    private String pgTid;
+
+    @Column(length = 200)
+    private String paymentKey;
+
+    private Integer installmentMonths;
+
+    @Column(name = "paid_at")
+    private LocalDateTime paidAt;
+
+    @Column(length = 300)
+    private String cancelReason;
+
+    @Column(length = 300)
+    private String failureReason;
+
+    @Column(name = "cancelled_at")
+    private LocalDateTime cancelledAt;
 
 
 }
